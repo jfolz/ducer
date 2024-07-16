5034d4c6af7b0f39bcdf3e808f31396749b1feb9
Joachim Folz <joachim.folz@dfki.de>
2024-07-16 17:45:52 +0200

    int encode/decode functions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,6 +16,47 @@ use std::{
     sync::Arc,
 };
 
+#[pyfunction]
+fn encode_int<'py>(py: Python<'py>, i: u64) -> PyResult<Bound<'py, PyBytes>> {
+    // +3 for bits needed to encode length
+    let num_bits = u64::BITS - (i | 1).leading_zeros() + 3;
+    let num_bytes = (num_bits + 7) >> 3;
+    if num_bytes > 8 {
+        return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
+            "int {i} > 2305843009213693951"
+        )));
+    }
+    let enc = (i << 3) | ((num_bytes - 1) as u64);
+    let enc = enc.to_be_bytes();
+    let (_, enc) = enc.split_at((u64::BITS / 8 - num_bytes) as usize);
+    Ok(PyBytes::new_bound(py, enc))
+}
+
+#[pyfunction]
+fn decode_int<'py>(py: Python<'py>, data: &[u8]) -> PyResult<Bound<'py, PyTuple>> {
+    let num_bytes = ((*data
+        .last()
+        .ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("empty"))?
+        & 0b111)
+        + 1) as usize;
+    if num_bytes > data.len() {
+        return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            "incomplete int",
+        ));
+    }
+    let mut i = 0u64;
+    let n = data.len() - num_bytes;
+    let (rem, data) = data.split_at(n);
+    for &b in data {
+        i = (i << 8) | u64::from(b);
+    }
+    // 3 bits were used to encode the length
+    i >>= 3;
+
+    let rem = PyBytes::new_bound(py, rem).into_py(py);
+    Ok(PyTuple::new_bound(py, [rem, i.into_py(py)]))
+}
+
 const BUFSIZE: usize = 4 * 1024 * 1024;
 
 #[pyclass]
@@ -162,7 +203,7 @@ fn fill_map<'py, W: io::Write>(
         let tuple = obj.downcast::<PyTuple>()?;
         if tuple.len() != 2 {
             return Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
-                "Expected 2-tuple (key: bytes, value: int)",
+                "Expected tuple (key: bytes, value: int)",
             ));
         }
         let item0 = tuple.get_item(0)?;
@@ -223,6 +264,8 @@ fn open_map<'py>(data: &Bound<'py, PyAny>) -> PyResult<FstMap> {
 #[pymodule]
 fn _fst(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<VecBuffer>()?;
+    m.add_function(wrap_pyfunction!(encode_int, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_int, m)?)?;
     m.add_function(wrap_pyfunction!(build_map, m)?)?;
     m.add_function(wrap_pyfunction!(open_map, m)?)?;
     Ok(())
