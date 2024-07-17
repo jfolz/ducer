7e64493ecbb873ac6969a00617fdd5b9d7ee99c9
Joachim Folz <joachim.folz@dfki.de>
2024-07-17 12:47:07 +0200

    cleanup Buffer::__getbuffer__

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -64,58 +64,6 @@ struct Buffer {
     data: Vec<u8>,
 }
 
-/// # Safety
-///
-/// `view` must be a valid pointer to ffi::Py_buffer, or null
-/// `data` must outlive the Python lifetime of `owner` (i.e. data must be owned by owner, or data
-/// must be static data)
-unsafe fn fill_view_from_readonly_data(
-    view: *mut ffi::Py_buffer,
-    flags: c_int,
-    data: &[u8],
-    owner: Bound<'_, PyAny>,
-) -> PyResult<()> {
-    if view.is_null() {
-        return Err(PyBufferError::new_err("View is null"));
-    }
-
-    if (flags & ffi::PyBUF_WRITABLE) == ffi::PyBUF_WRITABLE {
-        return Err(PyBufferError::new_err("Object is not writable"));
-    }
-
-    (*view).obj = owner.into_ptr();
-
-    (*view).buf = data.as_ptr() as *mut c_void;
-    (*view).len = data.len() as isize;
-    (*view).readonly = 1;
-    (*view).itemsize = 1;
-
-    (*view).format = if (flags & ffi::PyBUF_FORMAT) == ffi::PyBUF_FORMAT {
-        let msg = CString::new("B").unwrap();
-        msg.into_raw()
-    } else {
-        ptr::null_mut()
-    };
-
-    (*view).ndim = 1;
-    (*view).shape = if (flags & ffi::PyBUF_ND) == ffi::PyBUF_ND {
-        &mut (*view).len
-    } else {
-        ptr::null_mut()
-    };
-
-    (*view).strides = if (flags & ffi::PyBUF_STRIDES) == ffi::PyBUF_STRIDES {
-        &mut (*view).itemsize
-    } else {
-        ptr::null_mut()
-    };
-
-    (*view).suboffsets = ptr::null_mut();
-    (*view).internal = ptr::null_mut();
-
-    Ok(())
-}
-
 #[pymethods]
 impl Buffer {
     unsafe fn __getbuffer__(
@@ -123,11 +71,61 @@ impl Buffer {
         view: *mut ffi::Py_buffer,
         flags: std::os::raw::c_int,
     ) -> PyResult<()> {
-        fill_view_from_readonly_data(view, flags, &slf.borrow().data, slf.into_any())
+        //fill_view_from_readonly_data(view, flags, &slf.borrow().data, slf.into_any())
+        if view.is_null() {
+            return Err(PyBufferError::new_err("Py_buffer must not be null"));
+        }
+        if (flags & ffi::PyBUF_WRITABLE) == ffi::PyBUF_WRITABLE {
+            return Err(PyBufferError::new_err("Buffer is read-only"));
+        }
+
+        let data = &slf.borrow().data;
+        // Pointer to the wrapped Vec
+        // This is safe, since slf owns the underlying Vec
+        (*view).buf = data.as_ptr() as *mut c_void;
+        (*view).len = data.len() as isize;
+        (*view).readonly = 1;
+        (*view).itemsize = 1;
+        (*view).ndim = 1;
+
+        // Python C-API:
+        // If set, this field MUST be filled in correctly.
+        // Otherwise, this field MUST be NULL.
+        (*view).format = if (flags & ffi::PyBUF_FORMAT) == ffi::PyBUF_FORMAT {
+            let msg = CString::new("B").unwrap();
+            msg.into_raw()
+        } else {
+            ptr::null_mut()
+        };
+
+        // Set 1D shape if requested
+        (*view).shape = if (flags & ffi::PyBUF_ND) == ffi::PyBUF_ND {
+            &mut (*view).len
+        } else {
+            ptr::null_mut()
+        };
+
+        // Set stride 1 if requested
+        (*view).strides = if (flags & ffi::PyBUF_STRIDES) == ffi::PyBUF_STRIDES {
+            &mut (*view).itemsize
+        } else {
+            ptr::null_mut()
+        };
+
+        (*view).suboffsets = ptr::null_mut();
+        (*view).internal = ptr::null_mut();
+
+        // bind to self to ensure slf lives long enough
+        (*view).obj = slf.into_ptr();
+        Ok(())
     }
 
     unsafe fn __releasebuffer__(&self, view: *mut ffi::Py_buffer) {
-        drop(CString::from_raw((*view).format));
+        // drop the format string, if any
+        let fmt = (*view).format;
+        if !fmt.is_null() {
+            drop(CString::from_raw(fmt));
+        }
     }
 }
 
