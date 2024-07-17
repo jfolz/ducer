ee665261a08d8f5d84086a2b847655aceea16e42
Joachim Folz <joachim.folz@dfki.de>
2024-07-17 13:57:19 +0200

    move encoding functions to module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,58 +1,15 @@
-use pyo3::{
-    prelude::*,
-    types::{PyBytes, PyTuple},
-};
+use pyo3::prelude::*;
 
 mod buffer;
+mod encoding;
 mod map;
-
-#[pyfunction]
-fn encode_int<'py>(py: Python<'py>, i: u64) -> PyResult<Bound<'py, PyBytes>> {
-    // +3 for bits needed to encode length
-    let num_bits = u64::BITS - (i | 1).leading_zeros() + 3;
-    let num_bytes = (num_bits + 7) >> 3;
-    if num_bytes > 8 {
-        return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
-            "int {i} > 2305843009213693951"
-        )));
-    }
-    let enc = (i << 3) | ((num_bytes - 1) as u64);
-    let enc = enc.to_be_bytes();
-    let (_, enc) = enc.split_at((u64::BITS / 8 - num_bytes) as usize);
-    Ok(PyBytes::new_bound(py, enc))
-}
-
-#[pyfunction]
-fn decode_int<'py>(py: Python<'py>, data: &[u8]) -> PyResult<Bound<'py, PyTuple>> {
-    let num_bytes = ((*data
-        .last()
-        .ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("empty"))?
-        & 0b111)
-        + 1) as usize;
-    if num_bytes > data.len() {
-        return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-            "incomplete int",
-        ));
-    }
-    let mut i = 0u64;
-    let n = data.len() - num_bytes;
-    let (rem, data) = data.split_at(n);
-    for &b in data {
-        i = (i << 8) | u64::from(b);
-    }
-    // 3 bits were used to encode the length
-    i >>= 3;
-
-    let rem = PyBytes::new_bound(py, rem).into_py(py);
-    Ok(PyTuple::new_bound(py, [rem, i.into_py(py)]))
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _fst(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<buffer::Buffer>()?;
-    m.add_function(wrap_pyfunction!(encode_int, m)?)?;
-    m.add_function(wrap_pyfunction!(decode_int, m)?)?;
+    m.add_function(wrap_pyfunction!(encoding::encode_int, m)?)?;
+    m.add_function(wrap_pyfunction!(encoding::decode_int, m)?)?;
     m.add_function(wrap_pyfunction!(map::map_from_iterable, m)?)?;
     m.add_function(wrap_pyfunction!(map::map, m)?)?;
     Ok(())
