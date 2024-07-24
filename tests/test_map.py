8e3b3680aea2cc8a614c7a68f28471b99b3a21f2
Joachim Folz <joachim.folz@dfki.de>
2024-07-24 18:38:02 +0200

    more Map tests

--- tests/test_map.py
+++ tests/test_map.py
@@ -5,14 +5,37 @@ import contextlib
 import ducer
 
 
-DICT = {b"key1": 123, b"key2": 456}
+S1 = "key1"
+K1 = S1.encode('utf-8')
+V1 = 123
+I1 = K1, V1
+
+S2 = "key2"
+K2 = S2.encode('utf-8')
+V2 = 456
+I2 = K2, V2
+
+S3 = "key3"
+K3 = S3.encode('utf-8')
+V3 = 789
+I3 = K3, V3
+
+SO = "other"
+KO = SO.encode('utf-8')
+VO = 123456789
+IO = KO, VO
+
+DICT12 = dict((I1, I2))
+DICT123 = dict((I1, I2, I3))
+DICT23 = dict((I2, I3))
+DICT12O = dict((I1, I2, IO))
 
 
-def build_map(source=DICT, path=":memory:"):
+def build_map(source=DICT12, path=":memory:"):
     return ducer.Map.build(source.items(), path)
 
 
-def create_map(source=DICT):
+def create_map(source=DICT12):
     return ducer.Map(build_map(source=source))
 
 
@@ -63,127 +86,127 @@ def test_map_len_mmap(tmp_path):
 
 def test_map_getitem_contained():
     m = create_map()
-    for k, v in DICT.items():
+    for k, v in DICT12.items():
         assert m[k] == v
 
 
 def test_map_getitem_missing():
     m = create_map()
     with pytest.raises(KeyError):
-        m[b"key3"]
+        m[K3]
 
 
 def test_map_get_contained():
     m = create_map()
-    for k, v in DICT.items():
+    for k, v in DICT12.items():
         assert m.get(k) == v
 
 
 def test_map_get_contained_default():
     m = create_map()
-    for k, v in DICT.items():
+    for k, v in DICT12.items():
         assert m.get(k, default=17) == v
 
 
 def test_map_get_missing():
     m = create_map()
-    assert m.get(b"key3") is None
+    assert m.get(K3) is None
 
 
 def test_map_get_missing_default():
     m = create_map()
-    assert m.get(b"key3", 17) == 17
+    assert m.get(K3, 17) == 17
 
 
 def test_map_iter():
     m = create_map()
-    for k1, k2 in zip(m, sorted(DICT)):
+    for k1, k2 in zip(m, sorted(DICT12)):
         assert k1 == k2
 
 
 def test_map_iter_mmap(tmp_path):
     with init_mmap(tmp_path) as m:
-        for k1, k2 in zip(m, sorted(DICT)):
+        for k1, k2 in zip(m, sorted(DICT12)):
             assert k1 == k2
 
 
 def test_map_keys():
     m = create_map()
-    for k1, k2 in zip(m.keys(), sorted(DICT)):
+    for k1, k2 in zip(m.keys(), sorted(DICT12)):
         assert k1 == k2
 
 
 def test_map_values():
     m = create_map()
-    for v1, v2 in zip(m.values(), (v for _, v in sorted(DICT.items()))):
+    for v1, v2 in zip(m.values(), (v for _, v in sorted(DICT12.items()))):
         assert v1 == v2
 
 
 def test_map_items():
     m = create_map()
-    for i1, i2 in zip(m.items(), sorted(DICT.items())):
+    for i1, i2 in zip(m.items(), sorted(DICT12.items())):
         assert i1 == i2
 
 
 def test_map_range():
     m = create_map()
-    for i1, i2 in zip(m.range(), sorted(DICT.items())):
+    for i1, i2 in zip(m.range(), sorted(DICT12.items())):
         assert i1 == i2
 
 
 def test_map_range_lt():
     m = create_map()
-    items = list(m.range(lt=b"key2"))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
+    items = list(m.range(lt=K2))
+    assert I1 in items
+    assert I2 not in items
 
 
 def test_map_range_le():
     m = create_map()
-    items = list(m.range(le=b"key1"))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
+    items = list(m.range(le=K1))
+    assert I1 in items
+    assert I2 not in items
 
 
 def test_map_range_gt():
     m = create_map()
-    items = list(m.range(gt=b"key1"))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
+    items = list(m.range(gt=K1))
+    assert I1 not in items
+    assert I2 in items
 
 
 def test_map_range_ge():
     m = create_map()
-    items = list(m.range(ge=b"key2"))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
+    items = list(m.range(ge=K2))
+    assert I1 not in items
+    assert I2 in items
 
 
 def test_map_range_lt_gt():
     m = create_map()
-    items = list(m.range(lt=b"key2", gt=b"key1"))
+    items = list(m.range(lt=K2, gt=K1))
     assert not items
 
 
 def test_map_range_le_gt():
     m = create_map()
-    items = list(m.range(le=b"key2", gt=b"key1"))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
+    items = list(m.range(le=K2, gt=K1))
+    assert I1 not in items
+    assert I2 in items
 
 
 def test_map_range_lt_ge():
     m = create_map()
-    items = list(m.range(lt=b"key2", ge=b"key1"))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
+    items = list(m.range(lt=K2, ge=K1))
+    assert I1 in items
+    assert I2 not in items
 
 
 def test_map_search_always():
     m = create_map()
     a = ducer.Automaton.always()
     items = list(m.search(a))
-    for i in DICT.items():
+    for i in DICT12.items():
         assert i in items
 
 
@@ -205,7 +228,7 @@ def test_map_search_never_complement():
     m = create_map()
     a = ducer.Automaton.never().complement()
     items = list(m.search(a))
-    for i in DICT.items():
+    for i in DICT12.items():
         assert i in items
 
 
@@ -213,77 +236,112 @@ def test_map_search_str():
     m = create_map()
     a = ducer.Automaton.str("key1")
     items = list(m.search(a))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
+    assert I1 in items
+    assert I2 not in items
 
 
 def test_map_search_str_complement():
     m = create_map()
     a = ducer.Automaton.str("key1").complement()
     items = list(m.search(a))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
+    assert I1 not in items
+    assert I2 in items
 
 
 def test_map_search_subsequence():
     m = create_map()
     a = ducer.Automaton.subsequence("k1")
     items = list(m.search(a))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
+    assert I1 in items
+    assert I2 not in items
 
 
 def test_map_search_subsequence_complement():
     m = create_map()
     a = ducer.Automaton.subsequence("k1").complement()
     items = list(m.search(a))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
+    assert I1 not in items
+    assert I2 in items
 
 
 def test_map_search_starts_with():
-    d = dict(DICT)
-    d.update({b"other": 789})
-    m = create_map(source=d)
+    m = create_map(source=DICT12O)
     a = ducer.Automaton.str("key").starts_with()
     items = list(m.search(a))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) in items
-    assert (b"other", 789) not in items
+    assert I1 in items
+    assert I2 in items
+    assert IO not in items
 
 
 def test_map_search_starts_with_complement():
-    d = dict(DICT)
-    d.update({b"other": 789})
-    m = create_map(source=d)
+    m = create_map(source=DICT12O)
     a = ducer.Automaton.str("key").starts_with().complement()
     items = list(m.search(a))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) not in items
-    assert (b"other", 789) in items
+    assert I1 not in items
+    assert I2 not in items
+    assert IO in items
 
 
 def test_map_search_union():
-    d = dict(DICT)
-    d.update({b"other": 789})
-    m = create_map(source=d)
+    m = create_map(source=DICT12O)
     a1 = ducer.Automaton.str("key1")
     a2 = ducer.Automaton.str("oth").starts_with()
     a = a1.union(a2)
     items = list(m.search(a))
-    assert (b"key1", 123) in items
-    assert (b"key2", 456) not in items
-    assert (b"other", 789) in items
+    assert I1 in items
+    assert I2 not in items
+    assert IO in items
 
 
 def test_map_search_intersection():
-    d = dict(DICT)
-    d.update({b"key3": 789})
-    m = create_map(source=d)
+    m = create_map(source=DICT123)
     a1 = ducer.Automaton.str("key1").complement()
     a2 = ducer.Automaton.str("key3").complement()
     a = a1.intersection(a2)
     items = list(m.search(a))
-    assert (b"key1", 123) not in items
-    assert (b"key2", 456) in items
-    assert (b"key3", 789) not in items
+    assert I1 not in items
+    assert I2 in items
+    assert I3 not in items
+
+
+def test_map_difference():
+    m1 = create_map(source=DICT123)
+    m2 = create_map(source=DICT23)
+    m = ducer.Map(ducer.Map.difference(":memory:", m1, m2))
+    items = list(m.items())
+    assert I1 in items
+    assert I2 not in items
+    assert I3 not in items
+
+
+def test_map_intersection():
+    m1 = create_map(source=DICT12)
+    m2 = create_map(source=DICT23)
+    m = ducer.Map(ducer.Map.intersection(":memory:", m1, m2))
+    items = list(m.items())
+    assert I1 not in items
+    assert I2 in items
+    assert I3 not in items
+
+
+def test_map_symmetric_difference():
+    m1 = create_map(source=DICT12)
+    m2 = create_map(source=DICT23)
+    m = ducer.Map(ducer.Map.symmetric_difference(":memory:", m1, m2))
+    items = list(m.items())
+    assert I1 in items
+    assert I2 not in items
+    assert I3 in items
+
+
+def test_map_union():
+    m1 = create_map(source=DICT12)
+    m2 = create_map(source=DICT23)
+    m = ducer.Map(ducer.Map.union(":memory:", m1, m2))
+    items = list(m.items())
+    assert I1 in items
+    assert I2 in items
+    assert I3 in items
+
+
+# TODO test select=Op.*
